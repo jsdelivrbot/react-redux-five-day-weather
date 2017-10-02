1ee799edfce6be08c45f0f660cb491b40697d17a
Mark <lookandlearn76@yahoo.co.uk>
2017-10-02 13:28:53 +0100

    fixed heroku deploy bugs

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-var express = require('express');
+const express = require('express');
+const path = require('path');
 
 //create app
-var app = express();
-const PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.use(function (req, res, next){
-  if(req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-  } else {
-    next();
-  }
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
-
-app.use(express.static('public'));
 
 app.listen(PORT, function(){
   console.log('express server is up and running on port ' + PORT);
