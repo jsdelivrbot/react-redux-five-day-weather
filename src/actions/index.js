a51939ef77b25d6d804b976203a9361799f50dc6
Mark <lookandlearn76@yahoo.co.uk>
2017-10-02 14:06:52 +0100

    changed http request to https for weather fetch

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = '34631450ea80025535a7261f833f06f2';
 
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
@@ -10,7 +10,7 @@ export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},GB`;
   const request = axios.get(url);
 
-  
+
 
   return {
     type: FETCH_WEATHER,
